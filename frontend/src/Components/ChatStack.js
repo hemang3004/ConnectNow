2b3d9fb16eb30c9e67920b8aadccbe6b86fe761d
Hemang3004 <hemangparekh36@gmail.com>
2023-04-23 14:47:37 +0530

    FontWeight Turned back to normal after user click on it

--- frontend/src/Components/ChatStack.js
+++ frontend/src/Components/ChatStack.js
@@ -1,5 +1,5 @@
 import { Box, Stack, Text, useToast } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
 import { getSender } from "../config/ChatLogics";
 import { ChatState } from "../Context/ChatProvider";
 import CryptoJS from "crypto-js";
@@ -10,7 +10,7 @@ const ChatStack = ({ fetchAgain }) => {
   const { selectedChat, setSelectedChat, user, chats, setChats } = ChatState();
   const [loggedUser, setLoggedUser] = useState();
   const toast = useToast();
-
+  const refs=useRef([]);
   const decryptMsg = (key, message) => {
     return (message = CryptoJS.AES.decrypt(message, key).toString(
       CryptoJS.enc.Utf8
@@ -61,9 +61,14 @@ const ChatStack = ({ fetchAgain }) => {
   };
 
   useEffect(() => {
-    setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
     fetchChats();
   }, [fetchAgain]);
+  const testClick=(index)=>{
+   var arr=refs.current[index].querySelectorAll('p');
+   arr[2].style.fontWeight="normal"
+   arr[3].style.fontWeight="normal"
+
+  }
   return (
     <Stack overflowY="scroll">
       {chats.map((chat,index) =>{ 
@@ -76,7 +81,10 @@ const ChatStack = ({ fetchAgain }) => {
         });
         return (
         <Box
-          onClick={() => setSelectedChat(chat)}
+        ref={(el) => (refs.current[index] = el)}
+          onClick={() => {setSelectedChat(chat)
+          testClick(index)
+          }}
           cursor="pointer"
           // bg={selectedChat === chat ? "#4c6ed5" : "#E8E8E8"}
           // color={selectedChat === chat ? "white" : "black"}
