e1afa7a29812a1b162fa419119cf35be1e1978bc
Hemang3004 <hemangparekh36@gmail.com>
2023-04-27 20:38:18 +0530

    Change font weight when user see unread message

--- frontend/src/Components/ChatStack.js
+++ frontend/src/Components/ChatStack.js
@@ -1,16 +1,16 @@
 import { Box, Stack, Text, useToast } from "@chakra-ui/react";
-import React, { useEffect, useState,useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { getSender } from "../config/ChatLogics";
 import { ChatState } from "../Context/ChatProvider";
 import CryptoJS from "crypto-js";
 import axios from "axios";
 
-
 const ChatStack = ({ fetchAgain }) => {
   const { selectedChat, setSelectedChat, user, chats, setChats } = ChatState();
   const [loggedUser, setLoggedUser] = useState();
   const toast = useToast();
-  const refs=useRef([]);
+  const refs = useRef([]);
+
   const decryptMsg = (key, message) => {
     return (message = CryptoJS.AES.decrypt(message, key).toString(
       CryptoJS.enc.Utf8
@@ -61,62 +61,73 @@ const ChatStack = ({ fetchAgain }) => {
   };
 
   useEffect(() => {
+    setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
     fetchChats();
   }, [fetchAgain]);
-  const testClick=(index)=>{
-   var arr=refs.current[index].querySelectorAll('p');
-   arr[2].style.fontWeight="normal"
-   arr[3].style.fontWeight="normal"
-  }
+  const readMessageOnClick = (index) => {
+    var arr = refs.current[index].querySelectorAll("p");
+    arr[2].style.fontWeight = "normal";
+    arr[3].style.fontWeight = "normal";
+  };
   return (
     <Stack overflowY="scroll">
-      {chats.map((chat,index) =>{ 
-        var lastSeen="";
-        chat?.lastSeen.forEach(element => {
-          if(element.participant===String(user._id)){
-            lastSeen=element.lastTime
+      {chats.map((chat, index) => {
+        var lastSeen = "";
+        chat?.lastSeen.forEach((element) => {
+          if (element.participant === String(user._id)) {
+            lastSeen = element.lastTime;
           }
-          
         });
         return (
-        <Box
-        ref={(el) => (refs.current[index] = el)}
-          onClick={() => {setSelectedChat(chat)
-          testClick(index)
-          }}
-          cursor="pointer"
-          // bg={selectedChat === chat ? "#4c6ed5" : "#E8E8E8"}
-          // color={selectedChat === chat ? "white" : "black"}
-          bg={getChatBgColor(chat)}
-          color={getChatTextColor(chat)}
-          px={3}
-          py={2}
-          borderRadius="lg"
-          key={chat._id}
-        >
-          <Text>
-            {!chat.isGroupChat
-              ? getSender(loggedUser, chat.users)
-              : chat.chatName}
-          </Text>
-          {chat.latestMessage && (
-            <Text fontSize="xs">
-              <Text fontWeight={lastSeen<chat?.latestMessage?.time? "bold":"normal"} display={"inline"}>{chat.latestMessage.sender.name} : </Text>
-              <Text 
-               fontWeight={lastSeen<chat?.latestMessage?.time? "bold":"normal"} display={"inline"}
-              >
-              {chat.latestMessage.content.length > 50
-                ? decryptMsg(chat._id, chat.latestMessage.content).substring(
-                    0,
-                    51
-                  ) + "..."
-                : decryptMsg(chat._id, chat.latestMessage.content)}
-              </Text>
-              
+          <Box
+            ref={(el) => (refs.current[index] = el)}
+            onClick={() => {
+              setSelectedChat(chat);
+              readMessageOnClick(index);
+            }}
+            cursor="pointer"
+            // bg={selectedChat === chat ? "#4c6ed5" : "#E8E8E8"}
+            // color={selectedChat === chat ? "white" : "black"}
+            bg={getChatBgColor(chat)}
+            color={getChatTextColor(chat)}
+            px={3}
+            py={2}
+            borderRadius="lg"
+            key={chat._id}
+          >
+            <Text>
+              {!chat.isGroupChat
+                ? getSender(loggedUser, chat.users)
+                : chat.chatName}
             </Text>
-          )}
-        </Box>
-      )})}
+            {chat.latestMessage && (
+              <Text fontSize="xs">
+                <Text
+                  fontWeight={
+                    lastSeen < chat?.latestMessage?.time ? "bold" : "normal"
+                  }
+                  display={"inline"}
+                >
+                  {chat?.latestMessage?.sender?.name} :{" "}
+                </Text>
+                <Text
+                  fontWeight={
+                    lastSeen < chat?.latestMessage?.time ? "bold" : "normal"
+                  }
+                  display={"inline"}
+                >
+                  {chat.latestMessage.content.length > 50
+                    ? decryptMsg(
+                        chat._id,
+                        chat.latestMessage.content
+                      ).substring(0, 51) + "..."
+                    : decryptMsg(chat._id, chat.latestMessage.content)}
+                </Text>
+              </Text>
+            )}
+          </Box>
+        );
+      })}
     </Stack>
   );
 };
